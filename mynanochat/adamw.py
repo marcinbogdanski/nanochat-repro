dcd164e410c215e1cd18f0d4239599880291317c
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-01-30 19:59:10 +0000

    Update AdamW to match NanoChat non-DDP vesrion

--- mynanochat/adamw.py
+++ mynanochat/adamw.py
@@ -1,7 +1,7 @@
 import torch
 
-class Adam(torch.optim.Optimizer):
-    """Adam/AdamW optimizer
+class AdamW(torch.optim.Optimizer):
+    """AdamW optimizer
     
     Algorithm:
         v = B1 * v + (1 - B1) * g          # first moment (direction)
@@ -13,8 +13,8 @@ class Adam(torch.optim.Optimizer):
         p = p - lr * v_corrected / (sqrt(s_corrected) + eps)
         p = p - lr * wd * p                # AdamW: decoupled weight decay
     """
-    def __init__(self, params, lr=0.01, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, decoupled_weight_decay=False):
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, decoupled_weight_decay=decoupled_weight_decay)
+    def __init__(self, params, lr=0.01, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         super().__init__(params, defaults)
     
     @torch.no_grad()
@@ -26,7 +26,7 @@ class Adam(torch.optim.Optimizer):
                 # Lazy Init
                 if p not in self.state:
                     self.state[p] = {
-                        'step': 0,
+                        'step': torch.tensor(0, dtype=torch.int64, device=p.device),
                         'exp_avg': torch.zeros_like(p),
                         'exp_avg_sq': torch.zeros_like(p),
                     }
@@ -35,19 +35,14 @@ class Adam(torch.optim.Optimizer):
                 # Weight Decay
                 grad = p.grad
                 if group['weight_decay'] != 0.0:
-                    if not group['decoupled_weight_decay']:
-                        # Adam
-                        # equivalent to: loss = ... + weight_decay/2 * W**2
-                        grad = grad.add(p, alpha=group['weight_decay'])  # not in place!
-                    else:
-                        # AdamW
-                        # p = p - lr * weight_decay * p
-                        p.mul_(1 - group['lr'] * group['weight_decay'])
+                    # AdamW
+                    # p = p - lr * weight_decay * p
+                    p.mul_(1 - group['lr'] * group['weight_decay'])
 
                 # Update v
                 # v = B1 * v + (1-B1) * g
                 v = self.state[p]['exp_avg']
-                v.lerp_(grad, 1 - group['betas'][0])
+                v.mul_(group['betas'][0]).add_(grad, alpha=1-group['betas'][0])
 
                 # Update s
                 # s = B2 * s + (1-B2) * g**2
@@ -63,7 +58,7 @@ class Adam(torch.optim.Optimizer):
                 t = self.state[p]['step']
                 bias1 = 1-group['betas'][0]**t
                 bias2 = 1-group['betas'][1]**t
-                bias2_sqrt = bias2**0.5
-                denom = (s.sqrt() / bias2_sqrt).add_(group['eps'])
-                p.data.addcdiv_(v, denom, value=-group['lr'] / bias1)
+                denom = (s / bias2).sqrt().add_(group['eps'])
+                update = v.div(denom).mul_(group['lr'] / bias1)
+                p.data.add_(update, alpha=-1.0)
 
