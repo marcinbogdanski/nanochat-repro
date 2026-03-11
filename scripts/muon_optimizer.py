76d05ddab92ff503632500c50f177e00d42b41e8
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-03-11 21:53:13 +0000

    Fix DistMuon baseline profiler script

--- scripts/muon_optimizer.py
+++ scripts/muon_optimizer.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 from mynanochat.muon import Muon, DistMuon
 from mynanochat.muon_karpathy import Muon as MuonKarpathy
+from mynanochat.muon_karpathy import DistMuon as DistMuonKarpathy
 
 # Run like this
 # python -m scripts.muon_optimizer
@@ -67,23 +68,24 @@ def main():
         muon_groups.append({'params': group_params})
 
     # My version
-    muon_factory = DistMuon if ddp else Muon
-    muon_optimizer = muon_factory(
-        muon_groups,
-        lr=matrix_lr,
-        momentum=0.95,
-        ns_steps=5,
-        weight_decay=weight_decay,
-    )
-    # Karpathy version
-    # all_params = [p for group in muon_groups for p in group['params']]
-    # muon_optimizer = MuonKarpathy(
-    #     all_params,
+    # muon_factory = DistMuon if ddp else Muon
+    # muon_optimizer = muon_factory(
+    #     muon_groups,
     #     lr=matrix_lr,
     #     momentum=0.95,
     #     ns_steps=5,
     #     weight_decay=weight_decay,
     # )
+    # Karpathy version
+    muon_factory = DistMuonKarpathy if ddp else MuonKarpathy
+    all_params = [p for group in muon_groups for p in group['params']]
+    muon_optimizer = muon_factory(
+        all_params,
+        lr=matrix_lr,
+        momentum=0.95,
+        ns_steps=5,
+        weight_decay=weight_decay,
+    )
 
     mem_alloc = torch.cuda.memory_allocated() / (1024 ** 3)
     print0(f"Memory allocated after optimizer init: {mem_alloc:.2f} GB")
@@ -102,18 +104,24 @@ def main():
     mem_alloc = torch.cuda.memory_allocated() / (1024 ** 3)
     print0(f"Memory allocated after warmup: {mem_alloc:.2f} GB")
 
+    if ddp:
+        torch.distributed.barrier()
+    torch.cuda.synchronize()
 
     with torch.profiler.profile(
         activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
         record_shapes=True,
     ) as prof:
-        torch.cuda.synchronize()
         muon_optimizer.step()
         schedule_lr_etc(muon_optimizer)
         torch.cuda.synchronize()
-        muon_optimizer.step()
-        schedule_lr_etc(muon_optimizer)
 
+        muon_optimizer.step()
+        schedule_lr_etc(muon_optimizer)
+        torch.cuda.synchronize()
+
+    if ddp:
+        torch.distributed.barrier()
     torch.cuda.synchronize()
     ts = time.time()
 
@@ -123,6 +131,8 @@ def main():
         schedule_lr_etc(muon_optimizer)
     print0(" -------- HOT ITER END --------")
 
+    if ddp:
+        torch.distributed.barrier()
     torch.cuda.synchronize()
 
     te = time.time()
@@ -134,7 +144,7 @@ def main():
     print0("---")
     print0(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
     print0("---")
-    prof.export_chrome_trace(f"muon_optimizer_trace_rank{ddp_rank}.json")
+    prof.export_chrome_trace(f"muon_optimizer_trace_rank{ddp_rank}_k.json")
 
     # Print sum of all params to verify that they are changing
     total_sum = 0.0
