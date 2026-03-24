849497b6ecfd073ce680de4002c785030efacf36
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-03-24 00:05:17 +0000

    In to_fp8() scale properly in higher precision, baseline bf16

--- dev/example_fp8.py
+++ dev/example_fp8.py
@@ -6,26 +6,21 @@ torch.cuda.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 
 def to_fp8(x, dtype):
-    print('vvvvv')
     fp8_max = torch.finfo(dtype).max
-    print('fp8_max', fp8_max)
-    x_max = x.abs().amax()
-    print('x_max', x_max)
-    scale = fp8_max / x_max
-    print('scale', scale)
+    # Compute scale in higher precision
+    x_max = x.float().abs().amax()
+    # Go to fp63 so eager/compiled paths are numerically identical
+    scale = (fp8_max / x_max.double().clamp(min=1e-12)).float()
     x_scaled = x * scale
-    print('x_scaled', x_scaled)
+    # Expclict clamp, protect agains small numercial error if scale is imperfect
     x_scaled_clipped = x_scaled.clamp(-fp8_max, fp8_max)
-    print('x_scaled_clipped', x_scaled_clipped)
     x_fp8 = x_scaled_clipped.to(dtype)
-    print('x_fp8', x_fp8)
-    print('^^^^^')
-    return x_fp8, scale.reciprocal()
+    scale_inv = scale.reciprocal()
+    return x_fp8, scale_inv
 
-
-input = torch.randn(16, 64, device="cuda")
+input = torch.randn(16, 64, dtype=torch.bfloat16, device="cuda")
 print('input', input)
-weight = torch.randn(128, 64, device="cuda")
+weight = torch.randn(128, 64, dtype=torch.bfloat16, device="cuda")
 print('weight', weight)
 input_fp8, input_scale_inv = to_fp8(input, torch.float8_e4m3fn)
 print('input_fp8', input_fp8)
@@ -50,5 +45,10 @@ print('---')
 print(output_fp8)
 print('---')
 
-print( (output - output_fp8).abs().max() )
+abs_diff = (output - output_fp8).abs()
+rel_diff = abs_diff / output.abs().clamp_min(1e-6)
 
+print( f"{abs_diff.max()=}" )
+print( f"{abs_diff.mean()=}" )
+print( f"{rel_diff.max()=}")
+print( f"{rel_diff.mean()=}")
