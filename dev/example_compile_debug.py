1905650412a665bd3451cc4f69cd284304671929
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-03-25 22:59:32 +0000

    Update debug compile script to automatically setup and environment

--- dev/example_compile_debug.py
+++ dev/example_compile_debug.py
@@ -1,17 +1,20 @@
 import os
-if os.environ.get("RANK", "0") == "0":  # set on rank 0 only
-    os.environ["TORCH_LOGS"] = "graph_breaks,recompiles"
-else:
-    os.environ.pop("TORCH_LOGS", None)
-from contextlib import nullcontext
+import shutil
+
+# Create an empty TMPDIR, set TORCH_COMPILE_DEBUG, TORCH_LOGS
+# TMPDIR *must* be specified, and must exist, and must be empty - some lock/cache mechanism stops dump from working otherwise
+dirname = os.path.dirname(os.path.abspath(__file__))
+tmp_path = os.path.join(dirname, "../debug_tmp")
+shutil.rmtree(tmp_path, ignore_errors=True)
+os.makedirs(tmp_path)
+os.environ["TMPDIR"] = tmp_path
+os.environ["TORCH_COMPILE_DEBUG"] = "1"
+# os.environ["TORCH_LOGS"] = "graph_breaks,recompiles,+inductor"
 import torch
 import torch.nn as nn
 
 # Run like this:
-# TMPDIR=/workspace/my-nanochat/debug_tmp TORCH_COMPILE_DEBUG=1 TORCH_LOGS="+inductor" uv run python -m dev.example_debug
-# Notes:
-# TMPDIR *must* be specified, and must exist, and must be empty - some lock/cache mechanism stops dump from working otherwise
-
+# uv run python -m dev.example_debug
 
 class SmallModel(nn.Module):
     def __init__(self):
@@ -24,48 +27,15 @@ class SmallModel(nn.Module):
         x = self.linear2(x)
         return x
 
-
-
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-torch.cuda.manual_seed_all(42)
 x = torch.randn(16, 64, device="cuda")
 
 model = SmallModel().cuda()
 model = torch.compile(model)
 
 
-# autocast_ctx = nullcontext()
-autocast_ctx = torch.autocast(device_type='cuda', dtype=torch.bfloat16)
-
-with autocast_ctx:
+with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
     out = model(x)
 loss = out.float().square().mean()
 loss.backward()
-model.zero_grad()
 
-
-torch.cuda.synchronize()
-with torch.profiler.profile(
-        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-        record_shapes=True,
-    ) as prof:
-
-    with autocast_ctx:
-        out = model(x)
-    loss = out.float().square().mean()
-    #loss.backward()
-    #model.zero_grad()
-
-    torch.cuda.synchronize()
-
-
-
-
-print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
-prof.export_chrome_trace(f"trace.json")
-
-#print('---')
-#for i, e in enumerate(prof.events()[:150]):
-#    print(i, e.name, e.device_type, e.self_cpu_time_total)
-
+shutil.rmtree(tmp_path, ignore_errors=True)
