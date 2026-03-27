8cf5f153083e0ebb5a27e1e0006e6100c369677a
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-03-27 19:27:43 +0000

    In fp8.py add autocast handling, minor changes to align with Nanochat

--- mynanochat/fp8.py
+++ mynanochat/fp8.py
@@ -1,25 +1,28 @@
 import torch
 
+@torch.no_grad()
 def to_fp8(x, dtype):
     """Convert to one of fp8 dtypes"""
     assert dtype in [torch.float8_e4m3fn, torch.float8_e5m2]
 
     fp8_max = torch.finfo(dtype).max
     # Compute scale in higher precision
-    x_max = x.float().abs().amax()
+    x_max = x.float().abs().max()
     # Go to fp63 so eager/compiled paths are numerically identical
-    scale = (fp8_max / x_max.double().clamp(min=1e-12)).float()
-    x_scaled = x * scale
+    scale = fp8_max / x_max.double().clamp(min=1e-12)
+    scale = scale.float()
+    x_scaled = x.float() * scale
     # Explicit clamp, protect against small numerical error if scale is imperfect
     x_scaled_clipped = x_scaled.clamp(-fp8_max, fp8_max)
     x_fp8 = x_scaled_clipped.to(dtype)
     scale_inv = scale.reciprocal()
     return x_fp8, scale_inv
 
-
+# Allow treating as black box
+@torch._dynamo.allow_in_graph
 class FP8Matmul(torch.autograd.Function):
-    @staticmethod
     # ctx is the first argument to forward
+    @staticmethod
     def forward(ctx, input, weight):
         # The forward pass can use ctx.
         ctx.save_for_backward(input, weight)
@@ -53,7 +56,8 @@ class FP8Matmul(torch.autograd.Function):
                 mat2=weight_fp8_cont,
                 scale_a=grad_out_scale_inv,
                 scale_b=weight_scale_inv,
-                out_dtype=input.dtype,
+                #out_dtype=input.dtype,  # AI says this is more correct, but nanochat uses grad_output
+                out_dtype=grad_output.dtype,
                 use_fast_accum=False
             )
         
@@ -66,7 +70,8 @@ class FP8Matmul(torch.autograd.Function):
                 mat2=input_fp8_cont,
                 scale_a=grad_out_scale_inv,
                 scale_b=input_scale_inv,
-                out_dtype=weight.dtype,
+                #out_dtype=weight.dtype,  # AI says this is more correct, but nanochat uses grad_output
+                out_dtype=grad_output.dtype,
                 use_fast_accum=False
             )
 
@@ -75,6 +80,8 @@ class FP8Matmul(torch.autograd.Function):
 
 class FP8Linear(torch.nn.Linear):
     def forward(self, input):
+        if torch.is_autocast_enabled():
+            input = input.to(torch.get_autocast_gpu_dtype())
         input_2d = input.reshape(-1, input.shape[-1])
         output_2d = FP8Matmul.apply(input_2d, self.weight)
         output = output_2d.reshape(*input.shape[:-1], output_2d.shape[-1])
