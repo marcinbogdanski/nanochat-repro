fdb7005eafe3c290e7b9bc07495ae6c5882e4052
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-03-29 19:19:00 +0000

    Copy MoE implementation to the microbenchmark script

--- dev/microbenchmark_moe.py
+++ dev/microbenchmark_moe.py
@@ -1,7 +1,7 @@
 
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class SmallModelMLP(nn.Module):
     def __init__(self):
@@ -15,36 +15,58 @@ class SmallModelMLP(nn.Module):
         return x
     
 class SmallModelMOE(nn.Module):
-    def __init__(self):
+    def __init__(self, C, E, K):
         super().__init__()
-        self.experts1 = nn.ModuleList([nn.Linear(32, 128, bias=False) for _ in range(4)])
-        self.experts2 = nn.ModuleList([nn.Linear(128, 32, bias=False) for _ in range(4)])
+        self.K = K
+        self.router = nn.Linear(C, E, bias=False)
+        self.experts_up = nn.ModuleList([nn.Linear(C, C*4, bias=False) for _ in range(E)])
+        self.experts_down = nn.ModuleList([nn.Linear(C*4, C, bias=False) for _ in range(E)])
 
-    def call_experts_loop(self, x, experts, expert_offsets):
-        assert len(experts) == len(expert_offsets)
-        offsets_start = [0] + expert_offsets[:-1]
-        outputs = []
-        for expert, off_start, off_end in zip(experts, offsets_start, expert_offsets):
-            out = expert(x[off_start:off_end])
-            outputs.append(out)
-        outputs = torch.cat(outputs, dim=0)
-        B, T, C = x.shape
-        assert outputs.shape == (B, T, C*4)
-        return outputs
-    
+    torch.compiler.disable  # Dynamic slicing breaks the torch.compile
     def forward(self, x):
-        expert_offsets = [4, 8, 12, 16]  # index of end of group, non-inclusive, pass directly to grouped_mm()
-        x = self.call_experts_loop(x, self.experts1, expert_offsets=expert_offsets)
-        x = torch.relu(x)
-        x = self.call_experts_loop(x, self.experts2, expert_offsets=expert_offsets)
-        return x
+        B, T, C = x.shape
+        K = self.K
+        E = len(self.experts_up)
+        x_flat = x.reshape(-1, C)          # B*T, C
+        logits = self.router(x_flat)       # B*T, E
+        weights = torch.sigmoid(logits)    # B*T, E
+        values, indices = torch.topk(weights, K, dim=-1)     # B*T, K
+        x_flat_stacked = torch.stack([x_flat]*K, dim=1)      # B*T, K, C
+        x_flat_stacked_flat = x_flat_stacked.reshape(-1, C)  # B*T*K, C
+        indices_flat = indices.reshape(-1)                   # B*T*K
+        indices_flat_sorted_indices = torch.argsort(indices_flat, stable=True)  # B*T*K
+        x_flat_stacked_flat_sorted = x_flat_stacked_flat[indices_flat_sorted_indices]  # B*T*K, C
+        
+        start_idx = 0
+        outs = []
+        for i in range(E):
+            num_expert = (indices==i).sum().item()
+            end_idx = start_idx + num_expert
+            h = self.experts_up[i](x_flat_stacked_flat_sorted[start_idx:end_idx])
+            z = F.relu(h).square()
+            o = self.experts_down[i](z)
+            outs.append(o)
+            start_idx += num_expert
+        out_flat_stacked_flat_sorted = torch.cat(outs)   # B*T*K, C
+        
+        out_flat_stacked_flat = torch.zeros(B*T*K, C, device=x.device, dtype=x.dtype)
+        out_flat_stacked_flat[indices_flat_sorted_indices] = out_flat_stacked_flat_sorted   # B*T*K, C
+        out_flat_stacked = out_flat_stacked_flat.reshape(B*T, K, C)
+        out_flat_stacked_weighted = out_flat_stacked * values.unsqueeze(-1)
+        out_flat = out_flat_stacked_weighted.sum(dim=1)   # B*T, C
+        outputs = out_flat.reshape(B, T, C)
+        return outputs
     
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 
-model = SmallModelMOE().cuda()
-x = torch.randn(2, 16, 32, device="cuda")  # B,T,C
+B, T, C = 5, 16, 32
+E = 4  # num experts
+K = 2  # top_k
+
+model = SmallModelMOE(C=C, E=E, K=K).cuda()
+x = torch.randn(B, T, C, device="cuda")  # B,T,C
 
 out = model(x)
 loss = out.float().square().mean()
