6dc37781f615efbe728101980b29b4a60690b35a
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-04-06 22:05:34 +0000

    Reduce 4->2 GPUs in train_d12.sh

--- train_d12.sh
+++ train_d12.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=4 -m scripts.base_train \
+CUDA_VISIBLE_DEVICES=1,3 OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
   -- --depth=12 --device-batch-size=16 \
   --eval-every=250 --core-metric-every=250 --sample-every=250 \
   --run=d12m
