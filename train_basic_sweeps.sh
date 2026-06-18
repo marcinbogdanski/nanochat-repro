e6238e68b2145d0c14415ea1b611c2bbb0c628ab
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-06-18 12:13:35 +0000

    Add param sweep script and notebook

--- train_basic_sweeps.sh
+++ train_basic_sweeps.sh
@@ -2,31 +2,31 @@
 set -euo pipefail
 
 # Matrix LR sweep
-# for LR in 010 015 020 025 030; do
-#   OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
-#     -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
-#     --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
-#     --log-every=10 --log-wandb-every=10 \
-#     --matrix-lr=0.$LR --run=sweep_d12_mlr_$LR
-# done
+for LR in 010 015 020 025 030 035; do
+  OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
+    -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
+    --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
+    --log-every=10 --log-wandb-every=10 \
+    --matrix-lr=0.$LR --run=sweep_d12_mlr_$LR
+done
 
 # Embedding LR sweep
-# for LR in 20 30 45; do
-#   OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
-#     -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
-#     --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
-#     --log-every=10 --log-wandb-every=10 \
-#     --embedding-lr=0.$LR --run=sweep_d12_embd_lr_$LR
-# done
+for LR in 20 30 45; do
+  OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
+    -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
+    --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
+    --log-every=10 --log-wandb-every=10 \
+    --embedding-lr=0.$LR --run=sweep_d12_embd_lr_$LR
+done
 
 # Warmdown Ratio sweep
-# for WR in 50 65 80; do
-#   OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
-#     -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
-#     --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
-#     --log-every=10 --log-wandb-every=10 \
-#     --warmdown-ratio=0.$WR --run=sweep_d12_warmdown_$WR
-# done
+for WR in 50 65 80; do
+  OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.base_train \
+    -- --depth=12 --target-param-data-ratio=12 --device-batch-size=16 \
+    --eval-every=250 --core-metric-every=99999999 --sample-every=-1 --save-every=99999999 \
+    --log-every=10 --log-wandb-every=10 \
+    --warmdown-ratio=0.$WR --run=sweep_d12_warmdown_$WR
+done
 
 # Batch Size sweep
 for BS in 262144 524288 1048576; do
