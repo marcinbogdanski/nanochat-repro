78c86d656745491b796763cc527c8ff80b9f0c7f
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-07-23 16:56:46 +0000

    In Muon, introduce EqMuon and Muon+, similar to Nanochat

--- nanorepro/muon.py
+++ nanorepro/muon.py
@@ -39,11 +39,19 @@ def fused_muon_step(
     v.lerp_(grad, 1 - momentum)
     # Nesterov look-ahead: vv = B*v + (1-B)*g
     grad = grad.lerp(v, momentum)
+    # BF16 for speed
+    X = grad.to(compute_dtype)
+
+    # -----------------------
+    # MuonEq row equilibrium
+    # https://arxiv.org/abs/2603.28254
+    target = X.float().norm(dim=(-2, -1), keepdim=True) / (X.size(-2)**0.5)
+    row_norm = X.float().norm(dim=-1, keepdim=True).clamp_min(1e-6)
+    X = X * (target / row_norm).to(X.dtype)
 
     # --------------------------------
     # Polar express orthogonalization
     # https://arxiv.org/pdf/2505.16932
-    X = grad.to(compute_dtype)
     # Ensure spectral norm is at most 1 (with 2% safety factor)
     X = X / (X.norm(dim=(-2, -1), keepdim=True) * 1.01 + 1e-6)
     if grad.size(-2) > grad.size(-1):
@@ -59,6 +67,12 @@ def fused_muon_step(
             B = b * A + c * (A @ A)
             X = a * X + B @ X
     grad = X
+
+    # -----------------------
+    # Muon+ renormalization
+    target_norm = min(grad.size(-2), grad.size(-1))**0.5
+    grad_norm = grad.float().norm(dim=(-2, -1), keepdim=True).clamp_min(1e-6)
+    grad = grad * (target_norm / grad_norm).to(grad.dtype)
 
     # ---------------------------------------------
     # Similar to NorMuon per row variance reduction
